9f9645bfed09f35a35bacb3af8e43be49466604f
Guilherme Paes <guy.paes176@gmail.com>
2026-03-17 15:02:20 -0300

    feat: atualiza componente de Navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -2,14 +2,27 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div id="navbar" className="flex flex-row p-4 gap-6">
-            <Link href="/" className="text-xl mr-4 cursor-pointer">
-                OxentePass
+          <div className="navbar">
+            <Link 
+              href="/"
+              className="text-xl mr-4 cursor-pointer"
+            >
+              OxentePass
             </Link>
 
-            <Link href="/login" className="text-lg cursor-pointer">
-                Login
+            <Link 
+              href="/login"
+              className="text-lg cursor-pointer"
+            >
+              Login
             </Link>
-        </div>
+
+            <Link 
+              href="/cidade"
+              className="text-lg cursor-pointer"
+            >
+              Cidades
+            </Link>
+          </div>
     );
 }
