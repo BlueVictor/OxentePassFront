3fa29e358b8745d4a6ba193c60c19154e8353fa4
Arthur de Sá Tenório <arthursa147@gmail.com>
2026-03-17 03:03:19 -0300

    feat: imagens na home imcompleto

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import { chamadaAPI } from "../backend/chamadaPadrao";
 import Link from 'next/link';
+import Image from "next/image";
 import "./globals.css";
 
 const pagina = 0
@@ -19,22 +20,70 @@ async function getEventos () {
   return response.content
 }
 
+async function getImagemEvento (idEvento: string) {
+  const response = await chamadaAPI(
+    `/evento/${idEvento}/imagens`,
+    "GET"
+  )
+
+  if (!response) {
+    console.error("Falha no carregamento de imagens")
+    return
+  }
+
+  if (response.empty == true){
+    return {
+      chaveS3: "/next.svg", 
+      nome: "imagem placeholder" 
+    }
+  }
+  else {
+    return response.content[0]
+  }
+}
+
+function getURL(dado: string) {
+  if (dado.charAt(0) == '/') {
+    return dado
+  }
+  else {
+    return `${process.env.NEXT_PUBLIC_AWS_BASE_LINK}${dado}`
+  }
+}
+
 export default async function home() {
   const eventos = await getEventos()
+
+  const eventosComImagem = await Promise.all(
+    eventos.map(async (evento: any) => {
+      const imagens = await getImagemEvento(evento.id)
+
+      return {
+        ...evento,
+        imagem: imagens
+      }
+    })
+  )
 
   return (
     <div>
       <main>
         <div className="titulo">Eventos</div>
-        <div className="flex flex-row mt-5 gap-4 justify-center">
-          {eventos.map((item: any) => (
+        <div className="flex flex-row flex-wrap mt-5 gap-4 justify-center">
+          {eventosComImagem.map((item: any) => (
             <Link
               key={item.id} 
               href={`/evento/${item.id}`}
               className="cursor-pointer"
             >
               <div className="evento-card">
-                {item.nome}
+                <Image
+                  src={getURL(item.imagem.chaveS3)}
+                  alt={item.imagem.nome}
+                  width={200}
+                  height={100}
+                />
+                <h2 className="text-lg">{item.nome}</h2>
               </div>
             </Link>
           ))}
